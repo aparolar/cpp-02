a6f55417c9a7737f306ffcaa239cff6cc6215ccf
aparolar <82216543+aparolar@users.noreply.github.com>
2022-11-02 14:47:05 +0100

    finished

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 10:02:27 by aparolar          #+#    #+#             */
-/*   Updated: 2022/10/10 12:26:02 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/11/02 14:00:30 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	main(void)
 	Point a(2.0, 1.0);
 	Point b(4.0, 10.0);
 	Point c(6.0, 7.0);
-	Point point(4.0, 14);
+	Point point(4.0, 10.1);
 
 	if (bsp(a, b, c, point))
 		std::cout << "the point it's inside of the triangle" << std::endl;
 	else
-		std::cout << "the point dont it's inside of the triangle" << std::endl;
+		std::cout << "the point it's not inside of the triangle" << std::endl;
 
 	return (0);
 }
